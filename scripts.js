eb97285bfb6502246e79ebe1115ced24edb13077
Dan Boudet <dboudet04@gmail.com>
2021-07-21 12:25:17 -0400

    added form handling to add customers to firebase db. not working 100%: adds to db, but does not re-call getCustomers, AND throws catch err message...

--- scripts.js
+++ scripts.js
@@ -1,14 +1,49 @@
-    // let hero = document.getElementById('hero')
-    // hero.innerText = "This is my new Hero text"
-
-    // let footer = document.querySelector('footer')
-    // footer.style.backgroundColor = 'gray'
-
-    function handleNameChange() {
-        console.log('Name Changed', event.target.value)
+function handleSubmit(event) {
+    event.preventDefault()
+    console.log('Submitted Form')
+    const form = document.querySelector('form')
+    const newCustomer = {
+        firstName: form.elements.firstName.value,
+        lastName: form.elements.lastName.value,
+        email: form.elements.email.value
     }
+    console.log(newCustomer)
+    fetch('https://danb-pet-boutique.web.app/customers', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newCustomer),
+    })
+    .then(response => response.json())
+    .then(message => {
+        form.innerHTML = `<h3>${message}</h3>`
+        getCustomers()
+    })
+    .catch(err => {
+        form.innerHTML = '<h3>Error sending customer</h3>'
+    })
+}
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        console.log('Submitted Form')
-    }
+// function handleNameChange(event) {
+//     console.log('Name Changed', event.target.value)
+// }
+
+
+function getCustomers() {
+    fetch('https://danb-pet-boutique.web.app/customers')
+    .then(response => response.json())
+    .then(data => {
+        const custDiv = document.getElementById('customers')
+        custDiv.innerHTML = ''
+        const custArray = data.map(customer => {
+            return `<article>
+            <h3>${customer.firstName} ${customer.lastName}</h3>
+            <p>${customer.email}</p>
+            </article>`
+        })
+        custDiv.innerHTML = custArray.join('<br>')
+    })
+    .catch(err => console.error(err))
+}
+getCustomers()
